6d9ab5a1dd7eecec7e5923b94ad85b5797d25efb
Elsa B <elsa.bjorling@gmail.com>
2020-11-20 20:18:47 +0100

    don't think I changed anything

--- # Relative humidity.py
+++ # Relative humidity.py
@@ -11,8 +11,8 @@ def relative_humidity(vapor_pressure, temp_kelvin):
     return RH
 
 def vapor_pressure_moisture(RH, temp_kelvin):
-    p_saturated=compute_p_saturated(A, B, temp_kelvin, C)
-    vapor_pressure_moisture=RH*p_saturated
+    p_saturated = compute_p_saturated(A, B, temp_kelvin, C)
+    vapor_pressure_moisture = RH * p_saturated
     return vapor_pressure_moisture
 
 #### initial conditions
@@ -25,8 +25,8 @@ def X_P_init(alpha, N, RH_init):
 def heat_transfer_coefficient(moisture_diffusivity, gas_viscosity, column_diameter, porosity_powder, gas_density, 
                               particle_density, flow_rate, particle_diameter, Mw, superficial_velocity, molar_concentration,
                               gas_heat_capacity):
-    superficial_mass_velocity, particle_surface_area, reynolds_number, k_gp= mass_transfer_coefficient(moisture_diffusivity, gas_viscosity, column_diameter, porosity_powder, gas_density, 
-                              particle_density, flow_rate, particle_diameter, Mw, superficial_velocity, molar_concentration)
+    superficial_mass_velocity, particle_surface_area, reynolds_number, k_gp= compute_mass_transfer_coefficient(moisture_diffusivity, gas_viscosity, column_diameter, porosity_powder, gas_density,
+                                                                                                               particle_density, flow_rate, particle_diameter, Mw, superficial_velocity, molar_concentration)
     j_m=0.61* reynolds_number ** -0.41
     h_GP=(j_m*gas_density*gas_heat_capacity*superficial_velocity)/((gas_heat_capacity*gas_viscosity/k_gp)**(2/3))
     return h_GP
