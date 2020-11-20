327b6deed4b8afbb8a7abec51f21fd35b0fcc8e4
Elsa B <elsa.bjorling@gmail.com>
2020-11-20 20:17:38 +0100

    added relative humididty, but needs to be fixed

--- define_functions.py
+++ define_functions.py
@@ -10,10 +10,9 @@ def compute_moisture_particle(moisture_particle, alpha, N, relative_humidity, dt
     return moisture_particle_current
 
 
-def compute_temperature_particle(temp_particle, constant, dt, conductivity, laplacian, density, alpha, moisture,
-                                 relative_humidity, N, heat_of_vaporization, heat_transfer_coefficient,
-                                 specific_surface,
-                                 temp_gas, heat_capacity):
+def compute_temperature_particle(
+        temp_particle, constant, dt, conductivity, laplacian, density, alpha, moisture, relative_humidity, N,
+        heat_of_vaporization, heat_transfer_coefficient, specific_surface, temp_gas, heat_capacity):
     conduction = conductivity * laplacian / density
     heat_of_sorption = constant * (relative_humidity - compute_equilibrium_moisture(alpha, moisture, N)) * \
                        heat_of_vaporization
@@ -37,9 +36,10 @@ def compute_moisture_gas(moisture_particle, alpha, N, relative_humidity, dt, con
     return moisture_particle_current
 
 
-def compute_temperature_gas(temp_particle, constant, dt, conductivity_gas, laplacian, density_gas, alpha, moisture, N,
-                            heat_capacity_vapor, relative_humidity, heat_transfer_coefficient, specific_surface,
-                            temp_gas, heat_capacity_wet_gas, velocity, temp_gradient, porosity, density_particle):
+def compute_temperature_gas(
+        temp_particle, constant, dt, conductivity_gas, laplacian, density_gas, alpha, moisture, N, heat_capacity_vapor,
+        relative_humidity, heat_transfer_coefficient, specific_surface, temp_gas, heat_capacity_wet_gas, velocity,
+        temp_gradient, porosity, density_particle):
     conduction = conductivity_gas * (1 - porosity) * laplacian
     heat_of_sorption = density_particle * porosity * constant * \
                        (relative_humidity - compute_equilibrium_moisture(alpha, moisture, N)) * heat_capacity_vapor * \
@@ -53,7 +53,7 @@ def compute_temperature_gas(temp_particle, constant, dt, conductivity_gas, lapla
     temp_gas += change_temperature * dt
     return temp_gas
 
-######################################### OTHER ########################################################################
+######################################### ONE-TIME USE #################################################################
 def volumetric_flow_rate_m3_per_second(volumetric_flow_rate_liters_per_minute):
     volumetric_flow_rate = volumetric_flow_rate_liters_per_minute / (60 * 10 ** 3)  # from l/min -> cubic meters per s
     return volumetric_flow_rate
@@ -68,18 +68,6 @@ def compute_velocity(volumetric_flow_rate_liters_per_minute, length, diameter, v
     return velocity
 
 
-def compute_equilibrium_moisture(alpha, moisture_particle, N):
-    f_of_x = 1 - np.exp(-alpha * moisture_particle ** N)
-    return f_of_x
-
-
-def compute_p_saturated(A, B, temp_kelvin, C):  # Double-checked and clear! :)
-    temp_celsius = temp_kelvin - 273.15
-    p_saturated = 10 ** (A - B / (temp_celsius + C))
-    p_saturated_pascal = p_saturated * 133.322  # Torr to Pascal
-    return p_saturated_pascal
-
-
 def spec_surface_area(particle_diameter, particle_density):
     # S_P in m^2/kg - assuming spherical particles S_P=surface_area/volume*density
     r = particle_diameter / 2
@@ -87,14 +75,25 @@ def spec_surface_area(particle_diameter, particle_density):
     return SSA
 
 
-# def molar_mass_moisture_kg(molar_mass_moisture):
-#     Mw = molar_mass_moisture / 1000
-#     return Mw
+def compute_initial_moisture_particle(alpha, N, relative_humidity):
+    moisture_particle = (-np.log(-(relative_humidity - 1))/alpha)**(1/N)
+    return moisture_particle
+
+# Unused
+def molar_mass_moisture_kg(molar_mass_moisture):
+    Mw = molar_mass_moisture / 1000
+    return Mw
 
 
-def mass_transfer_coefficient(moisture_diffusivity, gas_viscosity, column_diameter, porosity_powder, gas_density,
-                              particle_density, flow_rate, particle_diameter, Mw, superficial_velocity,
-                              molar_concentration):
+######################################### RECURRENT ####################################################################
+def compute_equilibrium_moisture(alpha, moisture_particle, N):
+    f_of_x = 1 - np.exp(-alpha * moisture_particle ** N)
+    return f_of_x
+
+
+def compute_mass_transfer_coefficient(
+        moisture_diffusivity, gas_viscosity, column_diameter, porosity_powder, gas_density, particle_density, flow_rate,
+        particle_diameter, Mw, superficial_velocity, molar_concentration):
     SSA = spec_surface_area(particle_diameter, particle_density)
     superficial_mass_velocity = (4 * gas_density * flow_rate) / (np.pi * column_diameter ** 2)  # G_0
     particle_surface_area = porosity_powder * particle_density * SSA  # a
@@ -106,11 +105,48 @@ def mass_transfer_coefficient(moisture_diffusivity, gas_viscosity, column_diamet
     return superficial_mass_velocity, particle_surface_area, reynolds_number, k_gp
 
 
+def compute_heat_transfer_coefficient(
+        moisture_diffusivity, gas_viscosity, column_diameter, porosity_powder, gas_density, particle_density, flow_rate,
+        particle_diameter, Mw, superficial_velocity, molar_concentration, gas_heat_capacity):
+    superficial_mass_velocity, particle_surface_area, reynolds_number, k_gp = \
+        compute_mass_transfer_coefficient(moisture_diffusivity, gas_viscosity, column_diameter, porosity_powder, gas_density,
+                                          particle_density, flow_rate, particle_diameter, Mw, superficial_velocity, molar_concentration)
+    j_m= 0.61 * reynolds_number ** -0.41
+    h_GP = (j_m * gas_density * gas_heat_capacity * superficial_velocity)/((gas_heat_capacity * gas_viscosity/k_gp)**(2/3))
+    return h_GP
+
+
+def compute_p_saturated(A, B, temp_kelvin, C):  # Double-checked and clear! :)
+    temp_celsius = temp_kelvin - 273.15
+    p_saturated = 10 ** (A - B / (temp_celsius + C))
+    p_saturated_pascal = p_saturated * 133.322  # Torr to Pascal
+    return p_saturated_pascal
+
+
+def compute_partial_pressure_moisture(relative_humidity, pressure_saturated): # c = molar_concentration
+    partial_pressure_moisture = 0.01 * relative_humidity * pressure_saturated
+    return partial_pressure_moisture
+
+
+# def compute_partial_pressure_moisture(molar_concentration, R_gas_constant, temp): # c = molar_concentration
+#     partial_pressure_moisture = R_gas_constant * temp * molar_concentration
+#     return partial_pressure_moisture
+
+
+# def compute_relative_humidity(partial_p_moisture, p_saturated):
+#     relative_humidity = partial_p_moisture/(p_saturated)
+#     if relative_humidity > 1:
+#         print('Relative humidity larger than 1, error!')
+#     return relative_humidity
+
 def compute_relative_humidity():
-    relative_humidity = 0.75
+    relative_humidity = 0.5
+    if relative_humidity > 1:
+        print('Relative humidity larger than 1, error!')
     return relative_humidity
 
 
-def compute_molar_concentration(relative_humidity, pressure_saturated, R, temperature):
-    molar_concentration = 0.01 * relative_humidity * pressure_saturated / (R * temperature)
+def compute_molar_concentration(relative_humidity, pressure_saturated, R, temp):
+    print(relative_humidity)
+    molar_concentration = 0.01 * relative_humidity * pressure_saturated / (R * temp)
     return molar_concentration
