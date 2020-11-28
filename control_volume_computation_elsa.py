abb1acf8c8857c3eff7134c04d7e68a0a055f0d6
Elsa B <elsa.bjorling@gmail.com>
2020-11-28 13:15:54 +0100

    RH and moisture particle

--- control_volume_computation_elsa.py
+++ control_volume_computation_elsa.py
@@ -40,30 +40,39 @@ for t in range(number_of_time_steps):
             moisture_particle_current = moisture_particle[y, x]
             moisture_gas_current = moisture_gas[y, x]
 
-            moisture_particle[y, x] = compute_moisture_particle(
-                moisture_particle[y, x], alpha_parameter, N, relative_humidity[x], dt, constant[x])
+            moisture_particle[y, x], moisture_gas[y, x + 1], moisture_difference = compute_moisture_particle(
+                moisture_particle_current, alpha_parameter, N, relative_humidity[x], dt, constant[x],
+                moisture_gas_current, particle_density, porosity_powder, gas_density)
+            if x == 0:
+                print('\nMoisture difference: ', moisture_difference)
+                print('Moisture particle: ', moisture_particle[y, x])
+                print('Moisture gas: ', moisture_gas[y, x])
 
-            temp_particle[y, x] = compute_temperature_particle(
-                temp_particle_current, constant[x], dt, conductivity_particle, laplacian, particle_density, alpha_parameter,
-                moisture_particle_current, relative_humidity[x], N, heat_of_vaporization, heat_transfer_coefficient,
-                specific_surface_area, temp_gas_current, particle_heat_capacity, x)
+            # temp_particle[y, x] = compute_temperature_particle(
+            #     temp_particle_current, constant[x], dt, conductivity_particle, laplacian, particle_density, alpha_parameter,
+            #     moisture_particle_current, relative_humidity[x], N, heat_of_vaporization, heat_transfer_coefficient,
+            #     specific_surface_area, temp_gas_current, particle_heat_capacity, x)
+            #
+            # temp_gas[y, x] = compute_temperature_gas(
+            #     temp_particle_current, constant[x], dt, conductivity_gas, laplacian, gas_density, alpha_parameter,
+            #     moisture_particle_current, N, moisture_vapor_heat_capacity, relative_humidity[x], heat_transfer_coefficient,
+            #     specific_surface_area, temp_gas_current, gas_heat_capacity, superficial_velocity, temp_gradient, porosity_powder,
+            #     particle_density, x)
 
-            temp_gas[y, x] = compute_temperature_gas(
-                temp_particle_current, constant[x], dt, conductivity_gas, laplacian, gas_density, alpha_parameter,
-                moisture_particle_current, N, moisture_vapor_heat_capacity, relative_humidity[x], heat_transfer_coefficient,
-                specific_surface_area, temp_gas_current, gas_heat_capacity, superficial_velocity, temp_gradient, porosity_powder,
-                particle_density, x)
-
-            moisture_gas[y, x] = compute_moisture_gas(
-                moisture_particle_current, moisture_gas_current, alpha_parameter, N, relative_humidity[x], dt, constant[x], superficial_velocity,
-                moisture_diffusivity, gradient_moisture, laplacian_moisture, gas_density, particle_density, porosity_powder, x)
+            # moisture_gas[y, x] = compute_moisture_gas(
+            #     moisture_particle_current, moisture_gas_current, alpha_parameter, N, relative_humidity[x], dt, constant[x], superficial_velocity,
+            #     moisture_diffusivity, gradient_moisture, laplacian_moisture, gas_density, particle_density, porosity_powder, x)
 
             ############################ UPDATE PARAMETERS #############################################################
             pressure_saturated[x] = compute_p_saturated(A, B, temp_gas[y, x], C)
             partial_pressure_moisture[x] = compute_partial_pressure_moisture(molar_concentration_moisture[x],
                                                                              R_gas_constant, temp_gas[y, x])
+            if x == 0:
+                print('RH computed: ', relative_humidity[x])
+            relative_humidity[x] = compute_relative_humidity(
+                moisture_particle_current, alpha_parameter, N, moisture_difference, dt, constant[x], superficial_velocity,
+                moisture_diffusivity, gradient_moisture, laplacian, gas_density, particle_density, porosity_powder, x)
 
-            relative_humidity[x] = compute_relative_humidity(partial_pressure_moisture[x], pressure_saturated[x])
 
             mass_transfer_coefficient[x] = compute_mass_transfer_coefficient(
                 moisture_diffusivity, gas_viscosity, column_diameter, porosity_powder, gas_density, particle_density,
@@ -71,14 +80,12 @@ for t in range(number_of_time_steps):
 
             constant[x] = mass_transfer_coefficient[x] * specific_surface_area * pressure_saturated[x]/pressure_ambient  # just some simplification
 
-print('Change in temp particles:\n', (temp_particle - temp_initial)[0, 0:5])
-print('Change in temp gas:\n', (temp_gas - temp_initial)[0, 0:5])
+# print('Change in temp particles:\n', (temp_particle - temp_initial)[0, 0:5])
+# print('Change in temp gas:\n', (temp_gas - temp_initial)[0, 0:5])
 
 print('Change in moisture particles:\n', (moisture_particle - moisture_particle_initial)[0, 0:5])
 print('Change in moisture gas:\n', (moisture_gas - relative_humidity_gas_initial)[0, 0:5])
 print('Moisture gas:', (moisture_gas)[0, 0:5])
-# TODO: moisture in gas keeps going down, becoming negative. This is since RH is not changing, since gas temp and molar
-#  concentration are not changing. How does c change?!
 
 # print(relative_humidity_gas_initial - moisture_gas)
 # print(temp_particle - temp_initial)
