b501954c02216c1c534fe1e8c9b53f701f28d900
Elsa B <elsa.bjorling@gmail.com>
2020-12-11 12:01:10 +0100

    Pretty plots, 26 deg temp change. Love. It. <3

--- ode_solver_space_elsa.py
+++ ode_solver_space_elsa.py
@@ -1,18 +1,17 @@
 import numpy as np
 from scipy.integrate import odeint
 import matplotlib.pyplot as plt
-from vectorized_define_functions import*
-from define_functions import*
-from test_main import*
+import matplotlib.patches as mpatches
+from vectorized_define_functions import *
+from test_main import *
 
 def moisture(moisture_matrix, t, space_step, n_space_steps, alpha, N, diffusivity, density_gas, density_particle,
              porosity, velocity, heat_transfer_coefficient, heat_capacity_vapor, heat_capacity_wet_gas,
              heat_capacity_particle, specific_surface_area, conductivity_gas, conductivity_particle, bed_length):
-
     moisture_gas_vector = moisture_matrix[0:n_space_steps]
-    moisture_particle_vector = moisture_matrix[n_space_steps:n_space_steps*2]
-    temp_gas_vector = moisture_matrix[n_space_steps*2:n_space_steps*3]
-    temp_particle_vector = moisture_matrix[n_space_steps*3:n_space_steps*4]
+    moisture_particle_vector = moisture_matrix[n_space_steps:n_space_steps * 2]
+    temp_gas_vector = moisture_matrix[n_space_steps * 2:n_space_steps * 3]
+    temp_particle_vector = moisture_matrix[n_space_steps * 3:n_space_steps * 4]
 
     ##################################### UPDATE PARAMETERS ############################################################
     equilibrium_state = compute_equilibrium_moisture_vector(alpha, moisture_particle_vector, N)
@@ -44,9 +43,9 @@ def moisture(moisture_matrix, t, space_step, n_space_steps, alpha, N, diffusivit
     change_moisture_diffusion_gas = diffusivity * density_gas * (1 - porosity) * laplacian_moisture_gas
 
     change_moisture_absorption_gas = - constant * density_particle * porosity * \
-                                 (relative_humidity - equilibrium_state)
+                                     (relative_humidity - equilibrium_state)
     change_m_gas = (change_moisture_diffusion_gas + change_moisture_absorption_gas) / (density_gas * (1 - porosity)) - \
-                      velocity * gradient_moisture_gas
+                   velocity * gradient_moisture_gas
 
     change_m_particle = constant * (relative_humidity - equilibrium_state)
 
@@ -62,7 +61,6 @@ def moisture(moisture_matrix, t, space_step, n_space_steps, alpha, N, diffusivit
     change_temp_gas = (conduction_gas + heat_of_sorption + heat_transfer) / \
                       (density_gas * (1 - porosity) * heat_capacity_wet_gas) - velocity * gradient_temp_gas
 
-
     conduction_particle = conductivity_particle * laplacian_temp_particle / particle_density
     heat_of_sorption = constant * (relative_humidity - equilibrium_state) * heat_of_vaporization
 
@@ -75,63 +73,93 @@ def moisture(moisture_matrix, t, space_step, n_space_steps, alpha, N, diffusivit
     # test = (np.concatenate([change_m_gas, change_m_particle, change_temp_gas, change_temp_particle]))
     return np.concatenate([change_m_gas, change_m_particle, change_temp_gas, change_temp_particle])
 
-space_steps = 5
-length = np. linspace(0, bed_length, space_steps)
-space_step = bed_length/space_steps
-t = np.linspace(0, 1000000, 500)
 
-moisture_gas_initial = np.zeros(space_steps) + moisture_gas_initial_bed
-moisture_particle_initial = np.zeros(space_steps) + moisture_particle_initial
-# all_moisture = (np.concatenate([moisture_gas_initial, moisture_particle_initial]))
+space_steps = 6
+length = np.linspace(0, bed_length, space_steps)
+space_step = bed_length / space_steps
+t = np.linspace(0, 500000, 500)
+
+moisture_gas_initial_all = np.zeros(space_steps) + moisture_gas_initial_bed
+moisture_particle_initial_all = np.zeros(space_steps) + moisture_particle_initial
 
 temp_gas_initial = np.zeros(space_steps) + temp_initial
 temp_particle_initial = np.zeros(space_steps) + temp_initial
 
-# system0 = np.array([moisture_gas_initial, moisture_particle_initial, temp_gas_initial, temp_particle_initial])
-system0 = np.concatenate([moisture_gas_initial, moisture_particle_initial, temp_gas_initial, temp_particle_initial])
+system0 = np.concatenate(
+    [moisture_gas_initial_all, moisture_particle_initial_all, temp_gas_initial, temp_particle_initial])
 
 system = odeint(moisture, system0, t, args=(
     space_step, space_steps, alpha_parameter, N, moisture_diffusivity, gas_density, particle_density, porosity_powder,
     gas_velocity, heat_transfer_coefficient_initial, moisture_vapor_heat_capacity, gas_heat_capacity,
     particle_heat_capacity, specific_surface_area, conductivity_gas, conductivity_particle, bed_length))
 
-# print(system)
+######################################### PLOTTING #####################################################################
+# Convert to easier-to-read units
 seconds = t[-1]
-hours = seconds/3600
+hours = seconds / 3600
+t /= 3600
+system[:, space_steps * 2:space_steps * 3] -= kelvin
+max_temp_gas = np.max(system[:, space_steps * 2:space_steps * 3])
 
-fig, ax = plt.subplots(5, 4)
+system[:, space_steps * 3:space_steps * 4] -= kelvin
+max_temp_particle = np.max(system[:, (space_steps * 3):(space_steps * 4)])
+
+print(f'Time computed is: {int(hours)} hours')
+print('Max temperature in gas is: {:.4f} degrees Celcius'.format(max_temp_gas))
+print('Max temperature in particles is: {:.4f} degrees Celcius'.format(max_temp_particle))
+
+fig, ax = plt.subplots(space_steps, 4)
+fig.suptitle(f'Moisture & temperature in sections ove time. Total time: {int(hours)} hours.', fontsize=14)
+ax[0, 0].set_title('Moisture Gas')
+ax[0, 1].set_title('Moisture Particle')
+ax[0, 2].set_title('Temp Gas')
+ax[0, 3].set_title('Temp Particle')
+
+# Set styles
 moisture_color = 'navy'
 temp_color = 'orangered'
-# ax1.plot(t, system[:, 0], label = 'Gas moisture')
-# ax2.plot(t, system[:, 1], label = 'Particle moisture')
-ax[0, 0].plot(t, system[:, 0], label = 'Gas moisture section 1', c = moisture_color)
-ax[1, 0].plot(t, system[:, 1], label = 'Gas moisture section 2', c = moisture_color)
-ax[2, 0].plot(t, system[:, 2], label = 'Gas moisture section 3', c = moisture_color)
-ax[3, 0].plot(t, system[:, 3], label = 'Gas moisture section 4', c = moisture_color)
-ax[4, 0].plot(t, system[:, 4], label = 'Gas moisture section 5', c = moisture_color)
+initial_line = 'dashed'
+initial_color = 'gray'
+saturated_color = 'lightcoral'
 
-ax[0, 1].plot(t, system[:, 5], label = 'Particle moisture section 1', c = moisture_color)
-ax[1, 1].plot(t, system[:, 6], label = 'Particle moisture section 2', c = moisture_color)
-ax[2, 1].plot(t, system[:, 7], label = 'Particle moisture section 3', c = moisture_color)
-ax[3, 1].plot(t, system[:, 8], label = 'Particle moisture section 4', c = moisture_color)
-ax[4, 1].plot(t, system[:, 9], label = 'Particle moisture section 5', c = moisture_color)
+counter = 0
+epsilon = 0.001
+for feature in range(4):
+    if feature == 0 or feature == 1:
+        current_color = moisture_color
+    else:
+        # print('yo')
+        current_color = temp_color
 
-ax[0, 2].plot(t, system[:, 10], label = 'Gas temp section 1', c = temp_color)
-ax[1, 2].plot(t, system[:, 11], label = 'Gas temp section 2', c = temp_color)
-ax[2, 2].plot(t, system[:, 12], label = 'Gas temp section 3', c = temp_color)
-ax[3, 2].plot(t, system[:, 13], label = 'Gas temp section 4', c = temp_color)
-ax[4, 2].plot(t, system[:, 14], label = 'Gas temp section 5', c = temp_color)
+    for step in range(space_steps):
+        ax[step, feature].plot(t, system[:, counter], c=current_color)
 
-ax[0, 3].plot(t, system[:, 15], label = 'Particle temp section 1', c = temp_color)
-ax[1, 3].plot(t, system[:, 16], label = 'Particle temp section 2', c = temp_color)
-ax[2, 3].plot(t, system[:, 17], label = 'Particle temp section 3', c = temp_color)
-ax[3, 3].plot(t, system[:, 18], label = 'Particle temp section 4', c = temp_color)
-ax[4, 3].plot(t, system[:, 19], label = 'Particle temp section 5', c = temp_color)
+        if feature == 0:
+            patch = mpatches.Patch(color=current_color, label=f'Y {step}')
+            ax[step, feature].set_ylim(moisture_gas_initial_bed - epsilon, moisture_gas_initial_in + epsilon)
+            ax[step, feature].hlines(moisture_gas_initial_bed, 0, t[-1], colors=initial_color, linestyles=initial_line)
+            ax[step, feature].hlines(moisture_gas_initial_in, 0, t[-1], colors=saturated_color, linestyles=initial_line)
 
-# ax[0, 2].set_ylim(293, 294)
+        elif feature == 1:
+            patch = mpatches.Patch(color=current_color, label=f'X {step}')
+            ax[step, feature].set_ylim(moisture_particle_initial - epsilon, moisture_particle_saturated + epsilon)
+            ax[step, feature].hlines(moisture_particle_initial, 0, t[-1], colors=initial_color, linestyles=initial_line)
+            ax[step, feature].hlines(moisture_particle_saturated, 0, t[-1], colors=saturated_color, linestyles=initial_line)
 
-# ax[1].legend(loc="best")
-# ax[2].legend(loc="best")
+        elif feature == 2:
+            patch = mpatches.Patch(color=current_color, label=f'TG {step}')
+            ax[step, feature].set_ylim(temp_initial - (kelvin + epsilon), max_temp_gas + 1)
+            ax[step, feature].hlines(temp_initial - kelvin, 0, t[-1], colors=initial_color, linestyles=initial_line)
+            ax[step, feature].hlines(max_temp_gas, 0, t[-1], colors=saturated_color, linestyles=initial_line)
+        else:
+            patch = mpatches.Patch(color=current_color, label=f'TP {step}')
+            ax[step, feature].set_ylim(temp_initial - (kelvin + epsilon), max_temp_particle + 1)
+            ax[step, feature].hlines(temp_initial - kelvin, 0, t[-1], colors=initial_color, linestyles=initial_line)
+            ax[step, feature].hlines(max_temp_particle, 0, t[-1], colors=saturated_color, linestyles=initial_line)
 
-# plt.title(f'Time: {hours} h')
+        ax[step, feature].legend(handles=[patch], loc="upper left")
+        ax[step, feature].grid()
+        # ax[step, feature].xaxis.grid()
+        counter += 1
+
 plt.show()
