ac92c7125aa7760a21f75efee542d5bfa21d01b1
andreaL14y <andrea.bertl@posteo.de>
2020-12-11 14:02:47 +0100

    working on

--- Implicit_solver.py
+++ Implicit_solver.py
@@ -6,7 +6,7 @@ import matplotlib
 import numpy as np
 import math
 from define_functions import*
-from test_main import*
+from define_parameters_OLD import*
 from copy import deepcopy
 
 matplotlib.rc('font', size=18)
@@ -17,7 +17,7 @@ NN = 101 #number of grid points
 L = float(0.2) #size of grid
 dx = L/(NN-1) #grid spacing
 dt = dx / superficial_velocity #time step
-nsteps = 1000 #number of time steps
+nsteps = 20 #number of time steps
 
 r1 = dt/dx**2 #assuming heat diffusion coefficient == 1
 r2 = dt/(2*dx)
@@ -51,50 +51,67 @@ r_mp = np.zeros(NN)
 A_tp = np.zeros((NN,NN))
 B_tp = np.zeros((NN,NN))
 b = np.zeros((NN))
-#define matrices A, B and b array
-for i in range(NN):
-    b_mp[i] = -constant[i]
-    r_mp[i] = constant[i]*relative_humidity[i]
-
-    if i==0:
-        A_tp[i,:] = [1 if j==0 else 0 for j in range(NN)]
-        B_tp[i,:] = [1 if j==0 else 0 for j in range(NN)]
-        b[i] = 0. #boundary condition at i=1
-    elif i==N-1:
-        A_tp[i,:] = [1-dt*b_mp[i]*0.5 if j==i else 0 for j in range(NN)]
-        B_tp[i,:] = [1+dt*b_mp[i]*0.5 if j==i else 0 for j in range(NN)]
-        b[i] = dt*r_mp[i] #boundary condition at i=NN
-    else:
-        A_tp[i,:] = [1-dt*b_mp[i]*0.5 if j==i else 0 for j in range(NN)]
-        B_tp[i,:] = [1+dt*b_mp[i]*0.5 if j==i else 0 for j in range(NN)]
-        b[i] = 0.5*dt*(r_mp[i]+r_mp[i-1])
-    
-    pressure_saturated[i]=compute_p_saturated(A, B, temperature_gas[i], C)
-        
-    relative_humidity[i]=compute_relative_humidity_from_Y(molar_mass_dry_air, molar_mass_moisture, pressure_ambient, moisture_gas[i], pressure_saturated[i])
-       
-    molar_concentration_moisture[i]=compute_molar_concentration(relative_humidity[i], pressure_saturated[i], R_gas_constant, temperature_gas[i])
-        
-    partial_pressure_moisture[i]=compute_partial_pressure_moisture(molar_concentration_moisture[i], R_gas_constant, temperature_gas[i])  
-
-    k_GP[i] = compute_mass_transfer_coefficient(
-            moisture_diffusivity, gas_viscosity, column_diameter, porosity_powder, gas_density, particle_density, 
-            flow_rate, particle_diameter, molar_mass_moisture, superficial_velocity, molar_concentration_moisture[i])[3]
-        
-    constant[i]=k_GP[i] * specific_surface_area * pressure_saturated[i] / pressure_ambient
-    
-
 
 #initialize grid
 x = np.linspace(0,1,NN)
 #initial condition
-u = np.zeros(NN)+moisture_particle_initial
+X_P = np.zeros(NN)+moisture_particle_initial
 #evaluate right hand side at t=0
-bb = B_tp.dot(u) + b
+bb = B_tp.dot(X_P) + b
 
-for j in range(nsteps):
-    #find solution inside domain
-    u = np.linalg.solve(A_tp,bb)
-    #update right hand side
-    bb = B_tp.dot(u) + b
-print(u)
+for t in range(nsteps):
+    position_gas = t * gas_velocity
+    reached_points = position_gas/dx
+
+    for j in range(int(min(NN, reached_points))):
+        b_mp[j] = -constant[j]
+        r_mp[j] = constant[j]*relative_humidity[j]
+
+        A_mp = np.diagflat([1.]+[1-dt*b_mp[j]*0.5 for j in range(NN-1)])
+        B_mp = np.diagflat([1.]+[1+dt*b_mp[j]*0.5 for j in range(NN-1)])
+        b = np.array([0.]+[0.5*dt*(r_mp[j]+r_mp[j-1]) for j in range(NN-2)]+[dt*r_mp[j]])
+
+        pressure_saturated[j]=compute_p_saturated(A, B, temperature_gas[j], C)
+        
+        relative_humidity[j]=compute_relative_humidity_from_Y(molar_mass_dry_air, molar_mass_moisture, pressure_ambient, moisture_gas[j], pressure_saturated[j])
+       
+        molar_concentration_moisture[j]=compute_molar_concentration(relative_humidity[j], pressure_saturated[j], R_gas_constant, temperature_gas[j])
+        
+        partial_pressure_moisture[j]=compute_partial_pressure_moisture(molar_concentration_moisture[j], R_gas_constant, temperature_gas[j])  
+
+        k_GP[j] = compute_mass_transfer_coefficient(
+            moisture_diffusivity, gas_viscosity, column_diameter, porosity_powder, gas_density, particle_density, 
+            flow_rate, particle_diameter, molar_mass_moisture, superficial_velocity, molar_concentration_moisture[j])[3]
+        
+        constant[j]=k_GP[j] * specific_surface_area * pressure_saturated[j] / pressure_ambient
+        
+        #find solution inside domain
+        X_P [:j] = (np.linalg.solve(A_mp,bb))[:j]
+        #update right hand side
+        print('b: ', b)
+        bb[:j] = (B_mp.dot(X_P) + b)[:j]
+#print(X_P)
+
+
+    #define matrices A, B and b array
+    # for i in range(NN):
+    #     b_mp[i] = -constant[i]
+    #     r_mp[i] = constant[i]*relative_humidity[i]
+
+    #     A_mp = np.diagflat([1.]+[1-dt*b_mp[i]*0.5 for i in range(NN-1)])
+    #     B_mp = np.diagflat([1.]+[1+dt*b_mp[i]*0.5 for i in range(NN-1)])
+    #     b = np.array([0.]+[0.5*dt*(r_mp[i]+r_mp[i-1]) for i in range(NN-2)]+[dt*r_mp[i]])
+
+    #     pressure_saturated[i]=compute_p_saturated(A, B, temperature_gas[i], C)
+        
+    #     relative_humidity[i]=compute_relative_humidity_from_Y(molar_mass_dry_air, molar_mass_moisture, pressure_ambient, moisture_gas[i], pressure_saturated[i])
+       
+    #     molar_concentration_moisture[i]=compute_molar_concentration(relative_humidity[i], pressure_saturated[i], R_gas_constant, temperature_gas[i])
+        
+    #     partial_pressure_moisture[i]=compute_partial_pressure_moisture(molar_concentration_moisture[i], R_gas_constant, temperature_gas[i])  
+
+    #     k_GP[i] = compute_mass_transfer_coefficient(
+    #         moisture_diffusivity, gas_viscosity, column_diameter, porosity_powder, gas_density, particle_density, 
+    #         flow_rate, particle_diameter, molar_mass_moisture, superficial_velocity, molar_concentration_moisture[i])[3]
+        
+    #     constant[i]=k_GP[i] * specific_surface_area * pressure_saturated[i] / pressure_ambient
