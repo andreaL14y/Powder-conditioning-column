698a840f9227289d6e46bad7bfa21088cd2cfd30
khkt111 <khkt111@seskscpg009.prim.scp>
2021-04-22 07:49:33 +0000

    Updated w rotation at some interval

---
+++ ode_version_2/plot_ode_functions_2.py
@@ -0,0 +1,179 @@
+import numpy as np
+import matplotlib
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+import matplotlib.patches as mpatches
+from matplotlib.widgets import Slider
+
+def plot_sections_over_time(
+        moisture_gas_vector, moisture_particle_vector, temp_gas_vector, temp_particle_vector, height_of_interest,
+        n_space_steps, discrete_time, moisture_gas_initial_bed, moisture_gas_initial_in, moisture_particle_initial,
+        moisture_particle_saturated, temp_min, kelvin, hours, max_temp_gas, max_temp_particle):
+    epsilon = 0.001
+    height_of_interest -= 1
+    fig, ax = plt.subplots(n_space_steps, 4, figsize=(20, 13))
+    fig.suptitle(f'Moisture & temperature in cylinder sections over time. Total time: {int(hours)} hours.', fontsize=16)
+    ax[0, 0].set_title('Moisture Gas')
+    ax[0, 1].set_title('Moisture Particle')
+    ax[0, 2].set_title('Temp Gas')
+    ax[0, 3].set_title('Temp Particle')
+
+    # Set styles
+    moisture_color = 'navy'
+    temp_color = 'orangered'
+    initial_line = 'dashed'
+    initial_color = 'gray'
+    saturated_color = 'lightcoral'
+
+    for step in range(n_space_steps):
+        ax[step, 0].set_ylabel(f'Section {step + 1}                      ', rotation=0, size='large')
+        ax[step, 0].plot(discrete_time, moisture_gas_vector[:, height_of_interest, step], c=moisture_color)
+        patch = mpatches.Patch(color=moisture_color, label=f'Y {step}')
+        ax[step, 0].set_ylim(moisture_gas_initial_bed - epsilon, moisture_gas_initial_in + epsilon)
+        ax[step, 0].hlines(moisture_gas_initial_bed, 0, discrete_time[-1], colors=initial_color,
+                           linestyles=initial_line)
+        ax[step, 0].text(hours * 4 / 5, moisture_gas_initial_bed + epsilon,
+                         ('{:.4f}'.format(moisture_gas_initial_bed)), ha='left', va='center')
+        ax[step, 0].hlines(moisture_gas_initial_in, 0, discrete_time[-1], colors=saturated_color,
+                           linestyles=initial_line)
+        ax[step, 0].text(1, moisture_gas_initial_in - epsilon, ('{:.4f}'.format(moisture_gas_initial_in)),
+                         ha='left', va='center')
+
+
+        ax[step, 1].plot(discrete_time, moisture_particle_vector[:, height_of_interest, step],
+                         c=moisture_color)
+        patch = mpatches.Patch(color=moisture_color, label=f'X {step}')
+
+        # Add lines max and min
+        ax[step, 1].hlines(moisture_particle_initial, 0, discrete_time[-1], colors=initial_color,
+                           linestyles=initial_line)
+        ax[step, 1].text(hours * 4 / 5, moisture_particle_initial + 2 * epsilon,
+                         ('{:.4f}'.format(moisture_particle_initial)), ha='left', va='center')
+
+        ax[step, 1].hlines(moisture_particle_saturated, 0, discrete_time[-1], colors=saturated_color,
+                           linestyles=initial_line)
+        ax[step, 1].text(1, moisture_particle_saturated - 2 * epsilon,
+                         ('{:.4f}'.format(moisture_particle_saturated)),
+                         ha='left', va='center')
+
+
+        ax[step, 2].plot(discrete_time, temp_gas_vector[:, height_of_interest, step], c=temp_color)
+        patch = mpatches.Patch(color=temp_color, label=f'{step}')
+        ax[step, 2].set_ylim(temp_min - 1, max_temp_gas + 1)
+        ax[step, 2].hlines(temp_min, 0, discrete_time[-1], colors=initial_color,
+                           linestyles=initial_line)
+        ax[step, 2].hlines(max_temp_gas, 0, discrete_time[-1], colors=saturated_color,
+                           linestyles=initial_line)
+        ax[step, 2].text(hours * 4 / 5, max_temp_gas - 0.7,
+                         ('{:.2f}'.format(max_temp_gas)), ha='left', va='center')
+
+
+        ax[step, 3].plot(discrete_time, temp_particle_vector[:, height_of_interest, step],
+                         c=temp_color)
+        patch = mpatches.Patch(color=temp_color, label=f'{step}')
+        ax[step, 3].set_ylim(temp_min - 1, max_temp_particle + 1)
+        ax[step, 3].hlines(temp_min, 0, discrete_time[-1], colors=initial_color,
+                           linestyles=initial_line)
+
+        ax[step, 3].hlines(max_temp_particle, 0, discrete_time[-1], colors=saturated_color,
+                           linestyles=initial_line)
+        ax[step, 3].text(hours * 4 / 5, max_temp_particle - 0.7,
+                         ('{:.2f}'.format(max_temp_particle)), ha='left', va='center')
+
+        # ax[step, feature].legend(handles=[patch], loc="lower center")
+        ax[step, 0].grid()
+        ax[step, 1].grid()
+        ax[step, 2].grid()
+        ax[step, 3].grid()
+    plt.savefig('system_over_time.pdf')
+    plt.show()
+
+def plot_heatmap(
+        moisture_gas_vector, moisture_particle_vector, temp_gas_vector, temp_particle_vector, height_of_interest,
+        n_space_steps, discrete_time, moisture_gas_initial_bed, moisture_gas_initial_in, moisture_particle_initial,
+        moisture_particle_saturated, temp_min, kelvin, hours, max_temp_gas, max_temp_particle):
+    time, height, length = np.shape(moisture_gas_vector)
+    n_plots = 5
+    plot_times = np.linspace(0, time-1, n_plots, dtype=int)
+    fig, ax = plt.subplots(n_plots, 4, figsize=(20, 13))
+
+    fig.suptitle(f'Moisture & temperature in cylinder at chosen times. Total time: {int(hours)} hours.', fontsize=16)
+    ax[0, 0].set_title('Moisture Gas')
+    ax[0, 1].set_title('Moisture Particle')
+    ax[0, 2].set_title('Temp Gas')
+    ax[0, 3].set_title('Temp Particle')
+
+    temp_color = 'Reds'
+    humidity_color = 'Blues'
+    for index, h in enumerate(plot_times):
+        ax[index, 0].set_ylabel(f'T: {int(h/(time-1) * hours)} hours                      ', rotation=0, size='large')
+        im = ax[index, 0].imshow(moisture_gas_vector[h, :, :], vmin=moisture_gas_initial_bed,
+                                 vmax=moisture_gas_initial_in, cmap=humidity_color)
+        fig.colorbar(im, ax=ax[index, 0])
+
+        ax[index, 1].imshow(moisture_particle_vector[h, :, :])
+        im = ax[index, 1].imshow(moisture_particle_vector[h, :, :], vmin=moisture_particle_initial,
+                                 vmax=moisture_particle_saturated, cmap=humidity_color)
+        fig.colorbar(im, ax=ax[index, 1])
+
+
+        ax[index, 2].imshow(temp_gas_vector[h, :, :])
+        im = ax[index, 2].imshow(temp_gas_vector[h, :, :], vmin=temp_min, vmax=max_temp_gas, cmap=temp_color)
+        fig.colorbar(im, ax=ax[index, 2])
+
+
+        ax[index, 3].imshow(temp_particle_vector[h, :, :])
+        im = ax[index, 3].imshow(temp_particle_vector[h, :, :], vmin=temp_min, vmax=max_temp_particle, cmap=temp_color)
+        fig.colorbar(im, ax=ax[index, 3])
+    plt.savefig('heatmap_chosen_times.pdf')
+    plt.show()
+
+def slide_heat_map(
+    moisture_gas_vector, moisture_particle_vector, temp_gas_vector, temp_particle_vector, temp_min, max_temp_particle,
+    max_temp_gas, moisture_particle_initial, moisture_particle_saturated, moisture_gas_initial_bed, moisture_gas_initial_in, hours):
+    # current layer index start with the first layer
+    idx = 0
+    time, height, length = np.shape(moisture_particle_vector)
+
+    # figure axis setup
+    fig, ax = plt.subplots(2, 2, figsize=(20, 13))
+    fig.subplots_adjust(bottom=0.15)
+
+    ax[0, 0].set_title('Moisture Gas')
+    ax[0, 1].set_title('Moisture Particle')
+    ax[1, 0].set_title('Temp Gas')
+    ax[1, 1].set_title('Temp Particle')
+
+    # display initial image
+    im_y = ax[0, 0].imshow(moisture_gas_vector[idx, :, :], cmap='Blues', vmin=moisture_gas_initial_bed, vmax=moisture_gas_initial_in)
+    fig.colorbar(im_y, ax=ax[0, 0])
+
+    im_x = ax[0, 1].imshow(moisture_particle_vector[idx, :, :], cmap='Blues', vmin=moisture_particle_initial, vmax=moisture_particle_saturated)
+    fig.colorbar(im_x, ax=ax[0, 1])
+
+    im_tg = ax[1, 0].imshow(temp_gas_vector[idx, :, :], cmap='Reds', vmin=temp_min, vmax=max_temp_gas)
+    fig.colorbar(im_tg, ax=ax[1, 0])
+
+    im_tp = ax[1, 1].imshow(temp_particle_vector[idx, :, :], cmap='Reds', vmin=temp_min, vmax=max_temp_particle)
+    fig.colorbar(im_tp, ax=ax[1, 1])
+
+    # setup a slider axis and the Slider
+    ax_depth = plt.axes([0.23, 0.02, 0.56, 0.04])
+    slider_depth = Slider(ax_depth, 'Time', 0, time-1, valinit=0)
+
+    fig.suptitle(f'Moisture & temperature in cylinder at time: {int(idx)} hours.', fontsize=16)
+    # update the figure with a change on the slider
+    def update_depth(val):
+        idx = int(round(slider_depth.val))
+        im_x.set_data(moisture_particle_vector[idx, :, :])
+        im_y.set_data(moisture_gas_vector[idx, :, :])
+        im_tp.set_data(temp_particle_vector[idx, :, :])
+        im_tg.set_data(temp_gas_vector[idx, :, :])
+        time_current = idx/time * hours
+        fig.suptitle(f'Moisture & temperature in cylinder at time: {int(time_current)} hours.',
+                     fontsize=16)
+
+    slider_depth.on_changed(update_depth)
+    plt.savefig('heatmap_slider.pdf')
+    plt.show()
---
+++ ode_version_2/v_ode_main_2.py
@@ -0,0 +1,110 @@
+from scipy.integrate import odeint
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from v_ode_functions_2 import *
+from plot_ode_functions_2 import *
+
+################################## CHOOSE DISCRETIZATION ###############################################################
+max_time = 400000                       # seconds
+n_space_steps = 10                      # MUST BE EVEN NUMBER
+n_height_steps = int(n_space_steps/2)
+resolution = 1000
+height_of_interest = 3
+
+######################################### SETUP ########################################################################
+values_per_feature = n_space_steps * n_height_steps
+space_step_size = bed_length / n_space_steps
+discrete_time = np.linspace(0, max_time, resolution)
+#print(discrete_time)
+
+moisture_gas_initial_all = np.zeros((n_height_steps, n_space_steps)) + moisture_gas_initial_bed
+moisture_particle_initial_all = np.zeros((n_height_steps, n_space_steps)) + moisture_particle_initial
+temp_gas_initial = np.zeros((n_height_steps, n_space_steps)) + temp_initial
+temp_particle_initial = np.zeros((n_height_steps, n_space_steps)) + temp_initial
+
+########################################## COMPUTE #####################################################################
+initial_system = np.concatenate(
+    [moisture_gas_initial_all.flatten(), moisture_particle_initial_all.flatten(),
+     temp_gas_initial.flatten(), temp_particle_initial.flatten()])
+
+n_rotations = int(max_time/rotation_time_interval)
+n_time_outputs_per_rotation = int(resolution / n_rotations)
+discrete_time = discrete_time[0:(n_rotations*n_time_outputs_per_rotation)]
+print("Number of rotations are:", n_rotations)
+print("Number of time outputs per rotation is:", n_time_outputs_per_rotation)
+print("Shape of time is:", discrete_time.shape)
+#print("Time is:", discrete_time)
+
+computed_system = np.zeros([n_rotations, n_time_outputs_per_rotation, 4 * values_per_feature])
+
+for rotation in range(n_rotations):
+    print(rotation)
+    start_time = rotation * rotation_time_interval
+    discrete_time_division = np.linspace(start_time, start_time + rotation_time_interval, n_time_outputs_per_rotation)
+    computed_system[rotation, :] = odeint(conditioning_column, initial_system, discrete_time_division, args=(space_step_size, n_space_steps, n_height_steps))
+
+    #initial_system = average
+    initial_system = computed_system[rotation, -1, :]
+
+    moisture_gas_avg = np.average(computed_system[rotation, -1, 0:values_per_feature])
+    initial_system[0:values_per_feature] = moisture_gas_avg
+
+    moisture_particle_avg = np.average(computed_system[rotation, -1, values_per_feature:(2 * values_per_feature)])
+    initial_system[values_per_feature:(2 * values_per_feature)] = moisture_particle_avg
+
+    temp_gas_avg = np.average(computed_system[rotation, -1, (2 * values_per_feature):(3 * values_per_feature)])
+    initial_system[(2 * values_per_feature):(3 * values_per_feature)] = temp_gas_avg
+
+    temp_particle_avg = np.average(computed_system[rotation, -1, (3 * values_per_feature):(4 * values_per_feature)])
+    initial_system[(3 * values_per_feature):(4 * values_per_feature)] = temp_particle_avg
+
+########################################## SPLIT #######################################################################
+moisture_gas_vector = computed_system[:, :, 0:values_per_feature]
+moisture_particle_vector = computed_system[:, :, values_per_feature:(values_per_feature * 2)]
+temp_gas_vector = computed_system[:, :, (values_per_feature * 2):(values_per_feature * 3)]
+temp_particle_vector = computed_system[:, :, (values_per_feature * 3):(values_per_feature * 4)]
+
+moisture_gas_vector = moisture_gas_vector.reshape(-1, n_height_steps, n_space_steps)
+print("Shape moisture gas vector:", moisture_gas_vector.shape)
+moisture_particle_vector = moisture_particle_vector.reshape(-1, n_height_steps, n_space_steps)
+temp_gas_vector = temp_gas_vector.reshape(-1, n_height_steps, n_space_steps)
+temp_particle_vector = temp_particle_vector.reshape(-1, n_height_steps, n_space_steps)
+
+############################################ PLOT ######################################################################
+# Convert to easier-to-read units
+seconds = max_time
+hours = seconds / 3600
+discrete_time /= 3600
+
+max_temp_gas = np.max(temp_gas_vector)
+max_temp_gas_index = np.where(temp_gas_vector == max_temp_gas)
+
+max_temp_particle = np.max(temp_particle_vector)
+max_moisture_gas = np.max(moisture_gas_vector)
+max_moisture_particle = np.max(moisture_particle_vector)
+max_moisture_particle_index = np.where(moisture_particle_vector == max_moisture_particle)
+
+print(f'Time computed is: {seconds} seconds = {int(hours)} hours = {int(hours/24)} days\n')
+print('Max humidity in gas is: {:.4f}'.format(max_moisture_gas))
+print('Saturated humidity in gas at inlet is: {:.4f}\n'.format(moisture_gas_initial_in))
+
+print('Max humidity in particles is: {:.4f}'.format(max_moisture_particle))
+print('This happens at time, height, length: ', max_moisture_particle_index[0], max_moisture_particle_index[1], max_moisture_particle_index[2])
+print('Saturated humidity in particles is: {:.4f} \n'.format(moisture_particle_saturated))
+
+print('Max temperature in gas is: {:.4f} degrees Celcius'.format(max_temp_gas))
+print('Max temperature in particles is: {:.4f} degrees Celcius\n'.format(max_temp_particle))
+
+plot_sections_over_time(
+    moisture_gas_vector, moisture_particle_vector, temp_gas_vector, temp_particle_vector, height_of_interest,
+    n_space_steps, discrete_time, moisture_gas_initial_bed, moisture_gas_initial_in, moisture_particle_initial,
+    moisture_particle_saturated, temp_min, kelvin, hours, max_temp_gas, max_temp_particle)
+
+plot_heatmap(
+    moisture_gas_vector, moisture_particle_vector, temp_gas_vector, temp_particle_vector, height_of_interest,
+    n_space_steps, discrete_time, moisture_gas_initial_bed, moisture_gas_initial_in, moisture_particle_initial,
+    moisture_particle_saturated, temp_min, kelvin, hours, max_temp_gas, max_temp_particle)
+
+slide_heat_map(
+    moisture_gas_vector, moisture_particle_vector, temp_gas_vector, temp_particle_vector, temp_min, max_temp_particle,
+    max_temp_gas, moisture_particle_initial, moisture_particle_saturated, moisture_gas_initial_bed, moisture_gas_initial_in, hours)
