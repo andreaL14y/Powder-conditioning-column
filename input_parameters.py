de516b8eef40dfb7c5e65bdb395ea53f85ac0959
khkt111 <khkt111@seskscpg009.prim.scp>
2021-04-22 14:29:17 +0000

    Parameters closer to real setting

--- input_parameters.py
+++ input_parameters.py
@@ -9,6 +9,7 @@ C = 233.426                                                                     
 
 # Material specific for gas and powder
 porosity_powder = 0.6
+porosity_powder = 0.055
 N = 1                                                                           # Parameter, assume 1
 alpha_parameter = 25                                                            # parameter, 10 < alpha < 100
 gas_density = 1                                                                 # kg/m^3
@@ -30,14 +31,22 @@ boiling_temp = kelvin + 100                                                     
 
 # Cylinder and flow specific
 bed_length = 0.2                                                                 # m
-column_diameter = 0.1                                                            # m
+# bed_length = 0.2 * 0.1                                                                 # m
+# column_diameter = 0.1                                                            # m
+column_diameter = 0.024                                                          # m
 cross_sectional_area = np.pi * (column_diameter/2)**2                            # m^2
-volumetric_flow_rate_liters_per_minute = 1                                       # l/min
-rotation_time_interval = 300 * 60                                                 # s after which to rotate
+# volumetric_flow_rate_liters_per_minute = 1                                       # l/min
+volumetric_flow_rate_liters_per_minute = 0.5                                     # l/min
+rotation_time_interval = 19 * 60                                                # s after which to rotate
 
-temp_initial = kelvin + 20                                                       # K, room temperature 20 degrees
-temp_walls = kelvin + 20                                                         # At cylinder walls, cooling
+# temp_initial = kelvin + 20                                                       # K, room temperature 20 degrees
+temp_initial = kelvin + 24                                                       # K, room temperature 20 degrees
+temp_walls = kelvin + 24                                                         # At cylinder walls, cooling
 relative_humidity_bed_initial = 0.2                                              # humidity in bed, starting condition
-relative_humidity_gas_initial = 0.9                                              # humidity of flowing gas
+# relative_humidity_gas_initial = 0.9                                              # humidity of flowing gas
+relative_humidity_gas_initial = 0.45                                             # humidity of flowing gas
 relative_humidity_gas_end = 0.2                                                  # humidity at end of cylinder
 pressure_ambient = 101325                                                        # atmospheric pressure, Pa
+
+mass_powder = porosity_powder * particle_density * bed_length * (column_diameter/2)**2 * 1000
+print("Mass powder is:", mass_powder, "g")
