39a73463a1eb5dc7d319e29a41c89f220f61a251
khkt111 <khkt111@seskscpg009.prim.scp>
2021-05-11 14:09:27 +0000

    Added glass transition curve

--- ode_version_2/find_k_from_data.py
+++ ode_version_2/find_k_from_data.py
@@ -6,8 +6,11 @@ matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 
 
-def monoExp(data, starting_point, k_parameter, rest):
+def crystallization_speed_curves(data, starting_point, k_parameter, rest):
     return starting_point * np.exp(-k_parameter * data) + rest
+
+def glass_transition_curve(weight_fraction_lactose, param_1, param_2):
+    return param_1 + 1/(np.exp(-param_2 * weight_fraction_lactose))
 
 
 ########################################### FROM FIG 4 #################################################################
@@ -23,18 +26,18 @@ data = np.array([enthalpy_2030, enthalpy_2080, enthalpy_6030, enthalpy_6080])
 
 ############################################# FITTING ##################################################################
 x2 = np.linspace(0, 20 * multiply, 200)
-parameters = np.zeros((4, 3))
+crystallization_parameters = np.zeros((4, 3))
 
 colors = ['green', 'black', 'red', 'hotpink']
 for set in range(4):
     initial_parameters = (4.6, .005, .5)  # start with values near those we expect
     enthalpy = data[set]
-    parameters[set], cv = scipy.optimize.curve_fit(monoExp, time, enthalpy, initial_parameters)
-    if parameters[set][2] < 0:
-        parameters[set][2] = 0
+    crystallization_parameters[set], cv = scipy.optimize.curve_fit(crystallization_speed_curves, time, enthalpy, initial_parameters)
+    if crystallization_parameters[set][2] < 0:
+        crystallization_parameters[set][2] = 0
 
-    starting_point, k_parameter, rest = parameters[set]
-    y2 = monoExp(x2, starting_point, k_parameter, rest)
+    crystallization_param_1, crystallization_param_2, crystallization_param_3 = crystallization_parameters[set]
+    y2 = crystallization_speed_curves(x2, crystallization_param_1, crystallization_param_2, crystallization_param_3)
 #     plt.plot(x2, y2, '-', color=colors[set], label="fitted")
 #     plt.plot(time, enthalpy, 'X', color=colors[set], label="Original data")
 #
@@ -48,3 +51,25 @@ for set in range(4):
 #       f'\nT 60, RH 60: \nStarting point: {parameters[3, 0]:.2f}, k: {parameters[3, 1]:.2f} and rest: {parameters[3, 2]:.2f}\n')
 # plt.legend()
 # plt.show()
+
+weight_fraction_lactose = np.array([0, 0.2, 0.4, 0.6, 0.8, 1])
+temperature = np.array([-135, -127, -112, -90, -50, 100])
+
+# perform the fit
+p0 = (-135, -1) # start with values near those we expect
+params, cv = scipy.optimize.curve_fit(glass_transition_curve, weight_fraction_lactose, temperature, p0)
+glass_transition_param_1, glass_transition_param_2 = params
+
+
+# plot the results
+# plt.plot(weight_fraction_lactose, temperature, '.', label="data")
+# plt.plot(weight_fraction_lactose, glass_transition_curve(weight_fraction_lactose, glass_transition_param_1, glass_transition_param_2), '--', label="fitted")
+# plt.title("Fitted Exponential Curve")
+#
+# x2 = np.linspace(0, 1, 100)
+# y2 = glass_transition_curve(x2, glass_transition_param_1, glass_transition_param_2)
+# plt.plot(x2, y2)
+#
+# plt.show()
+# inspect the parameters
+# print(f"Y = {m} + 1/(e^(-{t} * x))")
