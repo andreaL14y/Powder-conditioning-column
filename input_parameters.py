516c0a40217d44f3b5f672623a027b19cbb10b1e
khkt111 <khkt111@seskscpg009.prim.scp>
2021-06-15 11:45:09 +0000

    Equilibrium stuff moved

--- input_parameters.py
+++ input_parameters.py
@@ -1,4 +1,6 @@
 import numpy as np
+from moisture_content_equilibrium import alpha_parameter_am, N_am, alpha_parameter_cryst, N_cryst, \
+    compute_GAB_equilibrium_moisture_cryst, compute_GAB_equilibrium_moisture_am
 
 ########################################### PARAMETERS #################################################################
 # Constants
@@ -14,17 +16,17 @@ C = 233.426                                                                     
 glass_temp_water_1 = 136                                                        # glass transition water, Kelvin
 glass_temp_water_2 = 165                                                        # glass transition water alt 2, Kelvin
 gas_viscosity = 10 ** -5                                                        # mu_G, kg/(m*s)
-moisture_diffusivity = 10 ** -5                                                 # D_G, m^2/s
+moisture_diffusivity = 2.42 * 10 ** -5                                          # D_G, m^2/s
 molar_mass_moisture = 18/1000                                                   # kg/mol for water vapor
 molar_mass_dry_air = 28.97/1000                                                 # kg/mol
 
 moisture_vapor_heat_capacity = 2000                                             # J/(kg*K), C_PV, heat cap water vapor
-moisture_liquid_heat_capacity = 4000                                            # J/(kg*K), C_M, heat cap liquid water
+moisture_liquid_heat_capacity = 4200                                            # J/(kg*K), C_M, heat cap liquid water
 boiling_temp = kelvin + 100                                                     # for water
 gas_heat_capacity = 1000                                                        # C_P_WG, heat cap gas
 conductivity_gas = 0.01                                                         # W/(m*K), lambda, conductivity gas
 gas_density = 1                                                                 # kg/m^3
-
+# gas_density = 0.0048                                                            # kg/m^3 for pure water vapor! Here mixed w dry air, which is much heavier
 
 # Lactose
 glass_temp_lactose = 101 + kelvin                                               # Kelvin
@@ -38,31 +40,30 @@ conductivity_particle = 0.1                                                     
 
 # Material specific for gas and powder
 porosity_powder = 0.5
-N = 1                                                                           # Parameter, assume 1
-alpha_parameter = 25                                                            # parameter, 10 < alpha < 100
-
+# # N_am = 1                                                                           # Parameter, assume 1
+# N_am = N_am                                                                           # Parameter, assume 1
 
 # Variables
 amorphous_material_initial = 0.16                                               # 16 % am left in batch L6.5 before cond, page 82 thesis
 
-bed_length = 0.032                                                               # m
-bed_length = 0.024                                                               # m
-# bed_length = 0.07                                                               # m
-column_diameter = 0.024                                                          # m
-cross_sectional_area = np.pi * (column_diameter/2)**2                            # m^2
-volumetric_flow_rate_liters_per_minute = 0.5                                     # l/min
-volumetric_flow_rate_liters_per_minute = 0.06                                     # l/min
-rotation_time_interval = 19 * 60                                                 # s after which to rotate
-rotation_time_interval = 58 * 60                                                 # s after which to rotate
+bed_length = 0.032                                                              # m
+bed_length = 0.024                                                              # m
+bed_length = 0.07                                                               # m
+column_diameter = 0.024                                                         # m
+cross_sectional_area = np.pi * (column_diameter/2)**2                           # m^2
+volumetric_flow_rate_liters_per_minute = 0.5                                    # l/min
+volumetric_flow_rate_liters_per_minute = 0.06                                   # l/min
+volumetric_flow_rate_liters_per_minute = 1                                      # l/min
+rotation_time_interval = 19 * 60                                                # s after which to rotate
+rotation_time_interval = 58 * 60                                                # s after which to rotate
 
-temp_initial = kelvin + 35                                                       # K, room temperature 24 degrees
-temp_initial = kelvin + 24                                                       # K, room temperature 24 degrees
-temp_walls = temp_initial                                                         # At cylinder walls, cooling
-relative_humidity_bed_initial = 0.3                                              # humidity in bed, starting condition
-relative_humidity_gas_inlet = 0.45                                                # humidity of flowing gas
-relative_humidity_gas_end = 0.3                                                  # humidity at end of cylinder
-pressure_ambient = 101325                                                        # atmospheric pressure, Pa
+temp_initial = kelvin + 35                                                      # K, room temperature 24 degrees
+temp_initial = kelvin + 24                                                      # K, room temperature 24 degrees
+temp_walls = temp_initial                                                       # At cylinder walls, cooling
+relative_humidity_bed_initial = 0.2                                             # humidity in bed, starting condition
+relative_humidity_gas_inlet = 0.45                                              # humidity of flowing gas
+relative_humidity_gas_end = 0.2                                                 # humidity at end of cylinder
+pressure_ambient = 101325                                                       # atmospheric pressure, Pa
 
 mass_powder = (1 - porosity_powder) * particle_density * \
-              bed_length * (column_diameter/2)**2 * 1000                         # weight converted to g
-print(mass_powder, 'g')
+              bed_length * (column_diameter/2)**2 * 1000                        # weight converted to g
