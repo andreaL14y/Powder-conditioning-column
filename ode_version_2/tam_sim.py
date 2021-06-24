b00a3eefb5a37663c3c36883dffae003b90a7991
khkt111 <khkt111@seskscpg009.prim.scp>
2021-06-24 13:56:01 +0000

    NA

--- ode_version_2/tam_sim.py
+++ ode_version_2/tam_sim.py
@@ -2,6 +2,8 @@ from v_ode_functions_2 import*
 from glass_transition_curve import compute_glass_temp_mix, glass_temp_lactose, glass_temp_water_1
 counter = 0
 from art_functions import compute_entropy_of_activation_S, compute_enthalpy_of_activation_H, compute_crystal_growth_rate
+from moisture_content_equilibrium import compute_air_equilibrium
+from moisture_sorption_constant import compute_moisture_change
 
 ###################################### MAIN EQUATIONS (1-4) ############################################################
 def conditioning_column(moisture_matrix, t, temp_gas, relative_humidity):
@@ -17,36 +19,26 @@ def conditioning_column(moisture_matrix, t, temp_gas, relative_humidity):
     pressure_saturated = compute_p_saturated_vector(temp_gas)
 
     molar_concentration_moisture = compute_molar_concentration_vector(relative_humidity, pressure_saturated, temp_gas)
+    # mass_transfer_coefficient = compute_mass_flux_diffusion_vector(molar_concentration_moisture)
+    # print("New way:", mass_transfer_coefficient)
     mass_transfer_coefficient = compute_mass_transfer_coefficient_vector(molar_concentration_moisture)[3]
+    # print("Old way:", mass_transfer_coefficient)
+    mass_transfer_coefficient /= 200                                                                                    # TODO: CHANGED
+
     constant = mass_transfer_coefficient * specific_surface_area * pressure_saturated / pressure_ambient
 
-    m_test_sat = compute_moisture_particle_from_RH_cryst(relative_humidity)
-    saturation_test = compute_air_equilibrium_cryst(moisture_cryst_particle_saturated)
-    # equilibrium_state_cryst = compute_equilibrium_moisture_vector(moisture_particle_cryst)
-    equilibrium_state_cryst = compute_GAB_equilibrium_moisture_cryst(relative_humidity)      #TODO: new eq function
-    equilibrium_state_am = compute_GAB_equilibrium_moisture_am(relative_humidity)
-    y = compute_Y_from_RH(0.45, pressure_saturated)
+    # constant /= 130
 
-    # moisture_estimate = np.where(moistures_cryst_eq > moisture_particle_cryst)
-    moisture_estimate_cryst = np.argwhere(moistures_cryst_eq > moisture_particle_cryst)[0]
-    equilibrium_state_cryst_air = relative_humidities_eq[moisture_estimate_cryst]
+    # print(mass_transfer_coefficient, specific_surface_area, pressure_saturated)
+    # print("Pressure ambient", pressure_ambient)
+    # print("Constant", constant)
 
-    moisture_estimate_am = np.argwhere(moistures_am_eq > moisture_particle_am)[0]
-    equilibrium_state_am_air = relative_humidities_eq[moisture_estimate_am]
-
-    print(moisture_estimate_cryst, moistures_cryst_eq[moisture_estimate_cryst], equilibrium_state_cryst_air)
-    print(moisture_estimate_am, moistures_am_eq[moisture_estimate_am], equilibrium_state_am_air)
-
+    equilibrium_state_cryst_air = compute_air_equilibrium(moisture_particle_cryst, alpha_parameter_cryst, N_cryst)
+    equilibrium_state_am_air = compute_air_equilibrium(moisture_particle_am, alpha_parameter_am, N_am)
 
     ############################## UPDATE AMORPHOUS MATERIAL ###########################################################
-    print(' ')
-    print(diff_temp)
-    change_amorphous_material = compute_amorphicity(moisture_particle_am, temp_particle, amorphous_material, glass_transition_temp)     # will be negative
-    # print('Moisture particle', moisture_particle_am)
-    # print('Temp particle', temp_particle)
-    # print('Am amount', amorphous_material)
-    # print('Change am', change_amorphous_material)
-
+    change_amorphous_material = compute_amorphicity(moisture_particle_am, temp_particle, amorphous_material,
+                                                    glass_transition_temp)     # will be negative
 
     ##################################### UPDATE MOISTURE ##############################################################
     total_equilibrium_term = ((1 - amorphous_material) * (relative_humidity - equilibrium_state_cryst_air) +
@@ -55,31 +47,24 @@ def conditioning_column(moisture_matrix, t, temp_gas, relative_humidity):
     #TODO: stop sorbing after start of crystallization
     #TODO: include crystallization moisture?
 
-    gas_contact_fraction = 0.02         # TODO: MADE UP!
-    change_m_particle_cryst = gas_contact_fraction * constant * (relative_humidity - equilibrium_state_cryst_air)  # sorbing from gas; eq_state_cryst is the air moisture equilibrium for current moisture powder
+    change_m_particle_cryst = constant * (relative_humidity - equilibrium_state_cryst_air)  # sorbing from gas; eq_state_cryst is the air moisture equilibrium for current moisture powder
 
-    change_m_particle_am_sorption = gas_contact_fraction * constant * (relative_humidity - equilibrium_state_am_air)  # sorbing from gas; eq_state_am is the powder moisture equilibrium
-    change_m_particle_am_crystallization = change_amorphous_material * constant * (moisture_particle_am - moisture_particle_cryst)  # negative, losing moisture
-    # change_m_particle_am_crystallization = - (moisture_particle_am - moisture_particle_cryst)  # negative, losing moisture
+    # change_m_particle_am_sorption = constant * (relative_humidity - equilibrium_state_am_air)  # sorbing from gas; eq_state_am is the powder moisture equilibrium
+    # print("Old change:", change_m_particle_am_sorption)
+    change_m_particle_am_sorption = compute_moisture_change(relative_humidity, porosity_powder, moisture_diffusivity, 1)
+    print("New change:", change_m_particle_am_sorption)
 
-    exponent = np.exp(-5 * diff_temp + 1)
-    sigma = 1/(1 + exponent)
-    sigma_rev = exponent/(1 + exponent)
+    # change_m_particle_am_crystallization = change_amorphous_material * constant * (moisture_particle_am - moisture_particle_cryst)  # negative, losing moisture
+    change_m_particle_am_crystallization = - (moisture_particle_am - moisture_particle_cryst)  # negative, losing moisture
 
-    change_m_particle_am_crystallization *= sigma
-    change_m_particle_am_sorption *= sigma_rev * amorphous_material
-
-    # if diff_temp > 0:
-    #     change_m_particle_am_sorption = 0
-    # else:
-    #     change_m_particle_am_crystallization = 0
-
-    change_m_particle_am = change_m_particle_am_sorption + change_m_particle_am_crystallization
+    change_m_particle_am = change_m_particle_am_sorption #+ change_m_particle_am_crystallization * change_amorphous_material
 
     ##################################### UPDATE TEMP ##################################################################
 
-    heat_of_sorption_particle = constant * total_equilibrium_term * heat_of_sorption
-    heat_transfer_particle  = heat_transfer_coefficient * specific_surface_area * (temp_gas-temp_particle)
+    heat_of_sorption_particle = heat_of_sorption * constant * total_equilibrium_term
+    heat_transfer_particle  = heat_transfer_coefficient * specific_surface_area * (temp_gas-temp_particle)            # TODO: ORIGINAL
+    # heat_transfer_particle  = heat_transfer_coefficient * constant * (temp_gas-temp_particle)
+    # heat_transfer_particle  = 0
 
     heat_of_cryst_particle  = heat_of_crystallization * constant * (-change_amorphous_material)     # kind of copied from heat of sorption
 
@@ -95,7 +80,7 @@ def compute_amorphicity(
 
     # Only change if above Tg
     k_parameter = compute_k_vector(temp_particle_vector, moisture_particle_am_vector, glass_transition_temp_vector)
-    print('k is',k_parameter)
+
     change_amorphous_material = - k_parameter * amorphous_material_vector
     return change_amorphous_material
 
@@ -104,13 +89,14 @@ def compute_k_vector(temperature_vector, moisture_content_vector, glass_transiti
     k_param = compute_crystal_growth_rate(moisture_content_vector, temperature_vector)
     diff_temp = temperature_vector - glass_transition_temp_vector
 
-    # k_vector *= 1/(1 + np.exp(-3 * diff_temp - 3))          # Sigmoid to ramp up, TODO: change this, nothing <0
     sigma = 1/(1 + np.exp(-5 * diff_temp + 3))
+    sigma = 1/(1 + np.exp(-0.3 * diff_temp + 4))                                                                        # TODO: CHANGED
+
+    # (1)/(1+ℯ^(-0.3 x+4))
     k_param *= sigma
-    if diff_temp < 0:
-        k_param = 0
-    # elif diff_temp > 10:
-    #     print(diff_temp, 'is diff_temp')
+    # if diff_temp < 30:
+    #     k_param = 0
+    k_param /= 30                                                                                                       # TODO: CHANGED
     return k_param
 
 def normalize_data(data):
