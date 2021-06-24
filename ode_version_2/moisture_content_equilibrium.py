5236979f84afcbc1ec96b4b1651d0cb0cec34a3a
khkt111 <khkt111@seskscpg009.prim.scp>
2021-06-24 13:54:55 +0000

    Computing GAB, derivative, fitting alpha and N

--- ode_version_2/moisture_content_equilibrium.py
+++ ode_version_2/moisture_content_equilibrium.py
@@ -11,6 +11,7 @@ alpha_parameter_cryst = 1733                                                    
 
 ######################################## FUNCTION & DATA ###############################################################
 def compute_air_equilibrium(moisture_particle_vector, alpha, N):
+    # print(moisture_particle_vector)
     equilibrium_air = (1 - np.exp(-alpha * moisture_particle_vector ** N))
     return equilibrium_air
 
@@ -26,7 +27,7 @@ def compute_GAB_equilibrium_moisture_am(relative_humidity_vector):
     equilibrium_moisture_particle_vector = M0 * c * f * relative_humidity_vector / \
                                            ((1 - f * relative_humidity_vector) * (
                                                        1 - (1 - c) * f * relative_humidity_vector))
-
+    # f(x) = moisture_particle(relative_humidity) = kg/kg
     return equilibrium_moisture_particle_vector
 
 
@@ -42,15 +43,46 @@ def compute_GAB_equilibrium_moisture_cryst(relative_humidity_vector):
     equilibrium_moisture_particle_vector = M0 * c * f * relative_humidity_vector * H * H_prime / \
                                            ((1 - f * relative_humidity_vector) * (
                                                        1 + (c * H - 1) * f * relative_humidity_vector))
-
     return equilibrium_moisture_particle_vector
 
 
 def equilibrium_curve_fit(X, alpha, N):
     return 1 - np.exp(-alpha * X**N)
 
+def derivative_m_isotherm_cryst(water_activity):
+    M0 = 1.68 * 10 ** (-4)
+    c = 8.8
+    f = 0.878
+    h = 30
+
+    top = c * f * M0 * (c * f**2 * water_activity**2 - f**2 * water_activity**2 +1)
+    bottom = (f * water_activity - 1)**2 * (c * f * water_activity - f * water_activity + 1)**2
+
+    derivative = top/bottom
+    return derivative
+
+def derivative_m_isotherm_am(water_activity):
+    M0 = 0.0488
+    c = 3.23
+    f = 1.16
+    top = c * f * M0 * (c * f**2 * water_activity**2 - f**2 * water_activity**2 +1)
+    bottom = (f * water_activity - 1)**2 * (c * f * water_activity - f * water_activity + 1)**2
+
+    derivative = top/bottom
+    return derivative
+
+
+def compute_moisture_change_am(water_activity, porosity, diffusion, laplacian):
+    derivative = derivative_m_isotherm_am(water_activity)
+
+    top = diffusion * laplacian
+    bottom = (1 - porosity) * derivative + porosity
+    moisture_change = top/bottom
+    return moisture_change
+
 relative_humidities = np.linspace(0, 0.85, 10000)
 moistures_am = compute_GAB_equilibrium_moisture_am(relative_humidities)
+moistures_am_der = derivative_m_isotherm_am(relative_humidities)
 
 moistures_cryst = compute_GAB_equilibrium_moisture_cryst(relative_humidities)
 eq_air_cryst = compute_air_equilibrium(moistures_cryst, alpha_parameter_cryst, N_cryst)
@@ -65,20 +97,27 @@ alpha_parameter_am, N_am = params
 eq_air_am = equilibrium_curve_fit(moistures_am, alpha_parameter_am, N_am)
 
 ############################################ PLOTTING ##################################################################
-# fig, axs = plt.subplots(2, figsize=(15, 15))
-# fig.suptitle('Equilibrium GAB vs fitted curves')
-# axs[0].plot(relative_humidities, moistures_cryst, label='Moisture content')
-# axs[0].plot(eq_air_cryst, moistures_cryst, label='Fitted curve')
-# axs[0].legend()
-# axs[0].grid()
-# axs[0].set_title('Crystalline lactose')
+# fig, axs = plt.subplots(2, 2, figsize=(15, 15))
 #
-# axs[1].plot(relative_humidities, moistures_am, label='Moisture content')
-# axs[1].plot(eq_air_am, moistures_am, label='Fitted curve')
-# axs[1].legend()
-# axs[1].grid()
-# axs[1].set_ylim(0, 0.9)
-# axs[1].set_title('Amorhpous lactose')
+# fig.suptitle('Equilibrium GAB vs fitted curves')
+# axs[0, 0].plot(relative_humidities, moistures_cryst, label='Moisture content')
+# # axs[0, 0].plot(eq_air_cryst, moistures_cryst, label='Fitted curve')
+# axs[0, 0].legend()
+# axs[0, 0].grid()
+# axs[0, 0].set_title('Crystalline lactose')
+#
+# axs[1, 0].plot(relative_humidities, moistures_am, label='Moisture content')
+# # axs[1, 0].plot(eq_air_am, moistures_am, label='Fitted curve')
+# axs[1, 0].legend()
+# axs[1, 0].grid()
+# axs[1, 0].set_ylim(0, 0.9)
+# axs[1, 0].set_title('Amorhpous lactose')
+#
+# axs[1, 1].plot(relative_humidities, moistures_am_der, label='Moisture content')
+# axs[1, 1].legend()
+# axs[1, 1].grid()
+# axs[1, 1].set_ylim(0, 0.9)
+# axs[1, 1].set_title('Amorhpous lactose derivative')
 #
 # plt.xlabel('RH')
 # plt.ylabel('Moisture, g/g lactose')
